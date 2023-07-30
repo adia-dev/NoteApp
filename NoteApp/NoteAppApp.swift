d7e31373424dc0c50af6b0c3979a28642ba190ec
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-07-30 15:21:06 +0200

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Abdoulaye Dia on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
